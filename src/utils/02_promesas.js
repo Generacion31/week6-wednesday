5712a87b72c881e0e359f18096aaae90b844b468
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-10-05 01:09:05 -0300

    advances

--- src/utils/02_promesas.js
+++ src/utils/02_promesas.js
@@ -50,3 +50,39 @@ promiseFuture
   .finally(() => console.log('Termino la promesa ✅'))
 
 console.log('tengo prisa, ejecutar ya y no quiero esperar 5 segundos');
+
+
+//LLAMADO API
+/**API (Application Programming Interface ) 
+ 
+ *JSON -> (JavaScript  Object Notation) {propiedad:valor, propiedad2:valor2}, es un formato de intercambio ligero. 
+ Su notacion es dervida de los objectos en JS, es decir que nomenclatura es bastante similar a los objetos que ya conocemos.
+
+ * Es ampliamente utilzado para intercambiar informacion entre el front end con el backend.
+ *
+*/
+
+
+function consumirApiFetch() { //fetch = buscar
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      console.log(data)
+      for (const value of data) {
+        ul.innerHTML += `
+        <li>
+          <h3>${value.title}</h3>
+          <h4>${value.body}</h4>
+        </li>
+      `
+      }
+    })
+    .catch((error) => console.log(error))
+    .finally(() => console.log('termine de hacer la peticion ✅ '))
+}
+
+const ul = document.querySelector("#ul")
+
+consumirApiFetch()
